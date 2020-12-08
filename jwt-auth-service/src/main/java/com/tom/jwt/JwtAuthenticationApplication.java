2dfa6a25924587932fbd62f9fd3ff779d11fd5b7
Tom Hodgkinson <hdgknsn@gmail.com>
2020-12-08 10:44:38 -0500

    intiial commit

---
+++ jwt-auth-service/src/main/java/com/tom/jwt/JwtAuthenticationApplication.java
@@ -0,0 +1,22 @@
+package com.tom.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+public class JwtAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationApplication.class, args);
+	}
+
+
+//	@Bean
+//	public PasswordEncoder myPasswordEncoder() {
+//		return NoOpPasswordEncoder.getInstance();
+//	}
+
+}
---
+++ user-service/src/main/java/com/tom/user/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.tom.user.controller;
+
+import com.tom.user.viewObject.ResponseTemplateVO;
+import com.tom.user.entity.User;
+import com.tom.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/")
+    public User saveUser(@RequestBody User user){
+        log.info("Inside saveUser of UserController");
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseTemplateVO getUserWithDepartment(@PathVariable("id") Long userId){
+        log.info("Inside getUserWithDepartment of UserController");
+        return userService.getUserWithDepartment(userId);
+    }
+}
